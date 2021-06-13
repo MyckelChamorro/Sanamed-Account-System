3b3cc4b72c7b223ef45e5839bd65bc17a8c485c5
MyckelChamorro <mschamorro@espe.edu.ec>
2021-06-13 16:41:33 -0500

    change

--- 06-Code/SanamedAccountSystem/src/sanamedaccountsystem/SanamedAccountSystem.java
+++ 06-Code/SanamedAccountSystem/src/sanamedaccountsystem/SanamedAccountSystem.java
@@ -15,7 +15,7 @@ public class SanamedAccountSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hey equipo");
+        System.out.println("Hey equipo como estan??");
     }
     
 }
