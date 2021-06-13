7f8b1c4af3c3799b59070410d9b2124797e1028d
MyckelChamorro <mschamorro@espe.edu.ec>
2021-06-13 16:48:32 -0500

    add code

---
+++ 06-Code/SanamedAccountSystem/src/ec/edu/espe/SanamedAccountSystem/model/Client.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.SanamedAccountSystem.model;
+
+/**
+ *
+ * @author Esteban
+ */
+public class Client {
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the adress
+     */
+    public String getAdress() {
+        return adress;
+    }
+
+    /**
+     * @param adress the adress to set
+     */
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return the creditCard
+     */
+    public CreditCard getCreditCard() {
+        return creditCard;
+    }
+
+    /**
+     * @param creditCard the creditCard to set
+     */
+    public void setCreditCard(CreditCard creditCard) {
+        this.creditCard = creditCard;
+    }
+    private int id;
+    private String name;
+    private String adress;
+    private int phoneNumber;
+    private CreditCard creditCard;    
+    
+    public void payWithCard(CreditCard card){
+        
+    }
+    
+    public void payWithChas(){
+        
+    }
+}
---
+++ 06-Code/SanamedAccountSystem/src/ec/edu/espe/SanamedAccountSystem/model/Pharmacy.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.SanamedAccountSystem.model;
+
+/**
+ *
+ * @author Esteban Chablay EMCL. Java ESPE-DCCO
+ */
+public class Pharmacy {
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the branchOffice
+     */
+    public String getBranchOffice() {
+        return branchOffice;
+    }
+
+    /**
+     * @param branchOffice the branchOffice to set
+     */
+    public void setBranchOffice(String branchOffice) {
+        this.branchOffice = branchOffice;
+    }
+    private String name;
+    private String branchOffice;
+}
